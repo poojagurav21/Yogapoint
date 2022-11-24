e5df30c29849167188143c10e7f09d60598d048e
poojagurav21 <pooja.gurav@waiin.com>
2022-11-24 17:20:16 +0530

    Login backend and UI

---
+++ aspnet-core/src/Yogapoint.HttpApi.Host/Pages/Account/CustomLoginModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Account.Web.Pages.Account;
+using Volo.Abp.Account.Web;
+
+namespace Yogapoint.Pages.Account
+{
+    public class CustomLoginModel : LoginModel
+    {
+        public CustomLoginModel(IAuthenticationSchemeProvider schemeProvider,
+            IOptions<AbpAccountOptions> accountOptions,
+            IOptions<IdentityOptions> identityOptions)
+            : base(schemeProvider, accountOptions, identityOptions)
+        {
+        }
+    }
+}
